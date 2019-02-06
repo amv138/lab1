4b0473073fbc8d5135fc6d7910acc2f8a35e18b8
Angel Vasquez <amvasquez138@gmail.com>
2019-02-06 15:09:16 -0800

    Commit

---
+++ lab1/PhotoDetailsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoDetailsViewController.swift
+//  lab1
+//
+//  Created by Angel Vasquez on 2/6/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class PhotoDetailsViewController: UIViewController {
+
+    @IBOutlet weak var photoDetailVIew: UIImageView!
+        var image: UIImage!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            photoDetailVIew.image = image
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- lab1/PhotoViewController.swift
+++ lab1/PhotoViewController.swift
@@ -43,6 +43,8 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
             }
         }
         task.resume()
+        self.tableView.reloadData()
+     
         
 
     }
@@ -68,20 +70,25 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
         
         
         // Configure YourCustomCell using the outlets that you've defined.
-        
+       
         return cell
     }
 
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let vc = segue.destination as! PhotoDetailsViewController
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        vc.image = cell.imageView?.image
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
     }
-    */
+    
 
 }
